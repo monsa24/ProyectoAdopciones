600c7daa33186303ba36d3f097bc3d4804044b29
Valeria Montoya <valems1996@gmail.com>
2024-08-29 17:44:37 -0600

    final

--- src/main/java/com/ProyectoAdopciones/dao/PerritoDao.java
+++
@@ -1,8 +0,0 @@
-package com.ProyectoAdopciones.dao;
-
-import com.ProyectoAdopciones.domain.Perrito;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PerritoDao extends JpaRepository <Perrito, Long> {
-
-}
